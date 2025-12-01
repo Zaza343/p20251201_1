ab7de7c00472a6dfc63b3090356a497b3d1b3bea
Zaza343 <fzalan2007@gmail.com>
2025-12-01 13:42:01 +0100

    Add project files.

---
+++ p20251201_1/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace p20251201_1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
